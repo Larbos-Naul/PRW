2c87e5e72a30bf6c318342f7bf06c76159329936
Sobral <luangabrielsobral160804@gmail.com>
2022-03-23 11:31:08 -0300

    exercicio de if e else chatão da ordem decrescente

---
+++ aula5/exercicio1.php
@@ -0,0 +1,25 @@
+<?php
+
+    $n1 = 3;
+    $n2 = 80;
+    $n3 = 1;
+
+    if ($n1 > $n2 and $n1 > $n3) //verifica se é o maior de todos
+        if($n2 > $n3)
+            echo 'A ordem decrescente correta é: ' .$n1 .', ' .$n2 .', ' .$n3;
+        else
+            echo 'A ordem decrescente correta é: ' .$n1 .', ' .$n3 .', ' .$n2;
+
+    elseif($n2 > $n1 and $n2 > $n3) //verifica se é o maior de todos
+        if($n1 > $n3)
+            echo 'A ordem decrescente correta é: ' .$n2 .', ' .$n1 .', ' .$n3;
+        else
+            echo 'A ordem decrescente correta é: ' .$n2 .', ' .$n3 .', ' .$n1;
+
+    elseif($n3 > $n1 and $n3 > $n2) //verifica se é o maior de todos
+        if($n1 > $n2)
+            echo 'A ordem decrescente correta é: ' .$n3 .', ' .$n1 .', ' .$n2;
+        else
+            echo 'A ordem decrescente correta é: ' .$n3 .', ' .$n2 .', ' .$n1;
+
+?>
