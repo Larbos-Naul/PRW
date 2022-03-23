397b836b7a32ff1ecbd68ca7297a7081e80ff84c
Sobral <luangabrielsobral160804@gmail.com>
2022-03-23 11:36:10 -0300

    aquele ex da ordem

--- aula5/exercicio1.php
+++ aula5/exercicio1.php
@@ -15,7 +15,7 @@
             echo 'A ordem decrescente correta é: ' .$n2 .', ' .$n1 .', ' .$n3;
         else
             echo 'A ordem decrescente correta é: ' .$n2 .', ' .$n3 .', ' .$n1;
-
+            
     elseif($n3 > $n1 and $n3 > $n2) //verifica se é o maior de todos
         if($n1 > $n2)
             echo 'A ordem decrescente correta é: ' .$n3 .', ' .$n1 .', ' .$n2;
