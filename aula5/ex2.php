b877e06731ac8302bccf567f6a25af87bcfb90c3
Sobral <luangabrielsobral160804@gmail.com>
2022-03-23 11:05:57 -0300

    exemplo de witch case

---
+++ aula5/ex2.php
@@ -0,0 +1,14 @@
+<?php
+
+    $op = "d";
+    
+    switch($op){
+        case 'a' : echo 'Abrir';
+            break;
+        case 'b': echo "Fechar";
+            break;
+        case 'c': echo "Salvar"; break;
+        case 'd': echo "Sair"; break;
+    }
+
+?>
