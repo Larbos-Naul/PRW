41475180e3750a7ab4951de19dd20e7aa86000da
Luan <luangabrielsobral160804@gmail.com>
2022-03-30 09:12:32 -0300

    sintaxe de data

--- aula6/exemplo_data.php
+++ aula6/exemplo_data.php
@@ -2,5 +2,5 @@
 
     //EXEMPLO DE FORMATAÇÃO DE DATA
 
-    echo "Data atual: " .date('d/m/y'); //Exibindo data no formato "date/month/year"
+    echo "Data atual: " .date('d/m/Y'); //Exibindo data no formato "date/month/year"
 ?>
