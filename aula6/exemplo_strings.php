3317f8728a74d57ebf351581bd4696111f28ebfc
Luan <luangabrielsobral160804@gmail.com>
2022-03-30 10:15:41 -0300

    exemplo de trim() e strlen()

--- aula6/exemplo_strings.php
+++ aula6/exemplo_strings.php
@@ -2,8 +2,9 @@
 
     //EXEMPLO DE MANIPULAÇÃO DE STRINGS identico ao slide
 
-    $palavra = 'Ciência da Computação ';
-    echo 'A variável contém: ' . strlen($palavra) . 'caracteres' . '<br>';
-    $palavra = trim($palavra);
-    echo 'A variável agora contém: ' . strlen($palavra) . 'caracteres';
+    $palavra = 'Ciência da Computação           ';
+    echo 'A variável contém: ' . strlen($palavra) . ' caracteres' . '<br>'; //strlen = tamanho da string
+
+    $palavra = trim($palavra); //trim = tamanho sem os espaços
+    echo 'A variável agora contém: ' . strlen($palavra) . ' caracteres';
 ?>
