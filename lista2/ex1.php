2879342923f9f034d9b3b81aa7d8cc0cdd663f66
Luan <luangabrielsobral160804@gmail.com>
2022-03-30 11:08:01 -0300

    exercicio 1 (soma dos pares menores que 100)

---
+++ lista2/ex1.php
@@ -0,0 +1,15 @@
+<?php
+
+    //Exercício 1 da lista 2
+
+    $num = 0;
+    $somapar = 0;
+
+    //foram usadas chaves para prevenção de quaisquer erros, o uso destas não era fulcral
+
+    for($num = 1; $num < 100; $num++){
+        if($num % 2 == 0){$somapar = $somapar + $num;}
+    }
+
+    echo 'A soma de todos os números pares menores que 100 é igual a: ' .$somapar;
+?>
