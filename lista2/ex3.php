21b6e16af0136bcb09d93a8d54614d50bff8fcf7
Luan <luangabrielsobral160804@gmail.com>
2022-03-30 11:40:17 -0300

    exercicio dos numeros primos (a terminar)

---
+++ lista2/ex3.php
@@ -0,0 +1,18 @@
+<?php
+
+    //Exercício 1 da lista 2
+
+    $num = 0;
+    $divisor = 0;
+    $primo = true;
+
+    for($num = 1; $num < 200; $num++){ 
+
+        for($divisor = 2; $divisor <= $num; $divisor++){
+            if($num % $divisor == 0)
+            
+            
+        }
+        
+    }
+?>
