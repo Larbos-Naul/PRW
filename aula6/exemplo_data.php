4c5b7e1eab6ba58eb59830e6076a0de3f7be9ed1
Luan <luangabrielsobral160804@gmail.com>
2022-03-30 09:19:16 -0300

    data e hora

--- aula6/exemplo_data.php
+++ aula6/exemplo_data.php
@@ -2,5 +2,6 @@
 
     //EXEMPLO DE FORMATAÇÃO DE DATA
 
-    echo "Data atual: " .date('d/m/Y'); //Exibindo data no formato "date/month/year"
+    echo "Data atual: " .date('d/m/Y') ."<br>"; //Exibindo data no formato "date/month/year"
+    echo "Hora atual: " .date('H:i:s'); //Exibindo horário no formato hora24/minutos/segundos
 ?>
