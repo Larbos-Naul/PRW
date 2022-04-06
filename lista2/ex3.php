63bc7d83f9d8f4d366583dd578a6bfbe4dd9e63d
Luan <luangabrielsobral160804@gmail.com>
2022-04-06 08:28:43 -0300

    Exercícios dos números primos

--- lista2/ex3.php
+++ lista2/ex3.php
@@ -6,10 +6,13 @@
     $divisor = 0;
     $primo = true;
 
-    for($num = 1; $num < 200; $num++){ 
+    for($num = 1; $num < 200; $num++){ //Repetição dos números a serem verificados
 
-        for($divisor = 2; $divisor <= $num; $divisor++){
-            if($num % $divisor == 0)
+        for($divisor = 2; $divisor <= $num; $divisor++){ //Verificar se o número não é primo, exibe se "não não for primo"
+            if($num % $divisor == 0){
+                break;
+            }
+            else {echo "$num <br>"}
             
             
         }
