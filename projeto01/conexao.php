57784178a658fa2b242cdaabc3a8eb010176d9f8
Luan <luangabrielsobral160804@gmail.com>
2022-04-20 10:52:05 -0300

    commit 20/04/22 (10:51am)

---
+++ projeto01/conexao.php
@@ -0,0 +1,19 @@
+<?php
+
+    //Fazer a conexão do php com o MySQL
+
+    $hostname = 'localhost';
+    $port = 3307;
+    $username = 'root';
+    $password = '';
+    $database = 'projeto01';
+
+    $con = mysqli_connect($hostname, $username, $password, $database, $port);
+    if(mysqli_connect_errno()){
+        printf("Erro ao conectar ao banco de dados: %s\n", mysqli_connect_error());
+        exit;
+    }
+
+    printf('Banco de dados conectado com sucesso! :D');
+
+?>
