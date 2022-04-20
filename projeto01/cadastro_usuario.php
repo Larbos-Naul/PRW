2d2bda09001ab85632826d701d084d574f824d92
Luan <luangabrielsobral160804@gmail.com>
2022-04-20 11:07:43 -0300

    commit

--- projeto01/cadastro_usuario.php
+++ projeto01/cadastro_usuario.php
@@ -1,5 +1,7 @@
 <?php
 
+    include('conexao.php'); //Inserir a esse programa as funionalidades de outro informado entre parênteses
+
     $nome = $_GET['nome'];
     $email = $_GET['email'];
     $tel = $_GET['telefone'];
