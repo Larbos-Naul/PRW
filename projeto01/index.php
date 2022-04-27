f31b59c846437157a5e4811cf8e548c6d3e57bd0
Luan <luangabrielsobral160804@gmail.com>
2022-04-27 10:35:40 -0300

    commit

---
+++ projeto01/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Menu Principal</title>
+
+    <link rel="stylesheet" href="estilo.css">
+
+</head>
+
+<body>
+    <h1>Menu Principal</h1>
+    <br>
+    <div id='menu'>
+        <ul>
+            <li><a href="cadastro_usuario.html">Cadastrar usuário</a></li>
+            <li><a href="listar_usuarios.php">Listar Usuário</a></li>
+        </ul>
+    </div>
+</body>
+</html>
