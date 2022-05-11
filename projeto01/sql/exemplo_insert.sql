b598d63658576b6a736ba5e696c70cbea0a1f492
luan <luangabrielsobral160804@gmail.com>
2022-05-11 10:43:37 -0300

    commit

--- projeto01/sql/exemplo_insert.sql
+++ projeto01/sql/exemplo_insert.sql
@@ -1,8 +1,8 @@
 use projeto01;
 
 -- insere esses dados na tabela lá
-INSERT INTO usuario (nome_usuario, email_usuario, telefone_usuario)
-VALUES ('Murilo Sauro', 'murilo@mail.com', '9090-7070');
+-- INSERT INTO usuario (nome_usuario, email_usuario, telefone_usuario)
+-- VALUES ('Murilo Sauro', 'murilo@mail.com', '9090-7070');
 
 -- insere os dados do formulario lá na tabela show
 SELECT * FROM usuario;
